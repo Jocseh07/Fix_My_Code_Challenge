306c1ad5e48e5b91776d309bb18892ce65085090
Jocseh07 <josephmkaranja07@gmail.com>
2024-05-15 21:49:16 +0300

    more

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,7 +3,7 @@
 """
 
 
-class square():
+class Square():
     """Square class."""
     width = 0
     height = 0
@@ -14,11 +14,11 @@ class square():
 
     def area_of_my_square(self):
         """ Area of the square."""
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
         """ Perimeter of the square."""
-        return (self.width * 2) + (self.width * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         """ Representation of square."""
@@ -28,7 +28,7 @@ class square():
 if __name__ == "__main__":
     """ Test the square."""
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
